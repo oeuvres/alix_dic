c03e837322ffbba22f810de02f8cb938b5dbe719
Frédéric Glorieux <frederic.glorieux@fictif.org>
2025-03-05 20:16:51 +0100

    Save

---
+++ words-build.php
@@ -0,0 +1,10 @@
+/**
+ # Alix, lexique pour lemmatisation
+
+* Convertir les étiquettes de grammalecte au format Alix
+* ? Pas de lemme dans la colonne lemme si c’est la ligne du lemme
+* Trier par ordre alphabétique, puis par étiquette
+* Trier les homographes comme word.csv pour les étiquettes
+* Lister les homographes inconnu
+
+*/
