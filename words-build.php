d66a800345b722bfe316df271c8040f66048b60e
Frédéric Glorieux <frederic.glorieux@fictif.org>
2025-03-07 16:31:13 +0100

    import cats from word

--- words-build.php
+++ words-build.php
@@ -45,3 +45,68 @@ function prenoms()
     }
 }
 
+function words()
+{
+    $words = [];
+    $handle = @fopen("word.csv", "r");
+    if ($handle) {
+        while (($line = fgets($handle, 4096)) !== false) {
+            if ($line[0] == '#') continue;
+            $data = str_getcsv($line, ";");
+            // take first one
+            $graph = $data[0];
+            if (isset($words[$graph])) continue;
+            $words[$graph] = $data[1];
+            /*
+            if ($data[3] != 1) continue;
+            $lem = $data[0];
+            $cat = $data[1];
+            if ($cat == 'VERBppas' || $cat == 'VERBger') {
+                continue;
+            }
+
+            if (isset($lemmata[$lem])) {
+                if ($lemmata[$lem] == $cat) continue;
+                $lemmata[$lem] = false;
+                continue;
+            }
+            else {
+                $lemmata[$lem] = $cat;
+            }
+            */
+        }
+        if (!feof($handle)) {
+            echo "Error: unexpected fgets() fail\n";
+        }
+        fclose($handle);
+    }
+    rename("grammalecte.tsv", "_grammalecte.tsv");
+    $read = @fopen("_grammalecte.tsv", "r");
+    $write = @fopen("grammalecte.tsv", "w");
+    if ($read) {
+        while (($line = fgets($read, 4096)) !== false) {
+            $data = str_getcsv($line, "\t");
+            $lem = $data[1];
+            $cat = $data[2];
+            if (
+                !isset($lemmata[$lem])
+                || !$lemmata[$lem]
+                || $cat == 'VERBppas'
+                || $cat == 'VERBger'
+            ) {
+                fwrite($write, $line);
+                continue;
+            }
+            $data[2] = $lemmata[$lem];
+            fwrite($write, implode("\t", $data) . "\n");
+        }
+        if (!feof($read)) {
+            echo "Error: unexpected fgets() fail\n";
+        }
+        fclose($read);
+        fclose($write);
+    }
+    unlink("_grammalecte.tsv");
+}
+
+words();
