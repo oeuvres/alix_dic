142197fe676bbfbc1197d71ca7872bc5c9294440
Frédéric Glorieux <frederic.glorieux@fictif.org>
2025-03-07 15:06:50 +0100

    Save

--- words-build.php
+++ words-build.php
@@ -1,3 +1,4 @@
+<?php
 /**
  # Alix, lexique pour lemmatisation
 
@@ -8,3 +9,39 @@
 * Lister les homographes inconnu
 
 */
+
+
+function prenoms()
+{
+    $forename = [];
+    $handle = @fopen("forename.csv", "r");
+    if ($handle) {
+        while (($line = fgets($handle, 4096)) !== false) {
+            if ($line[0] == '#') continue;
+            $data = str_getcsv($line, ",");
+            $forename[$data[0]] = $data[1];
+        }
+        if (!feof($handle)) {
+            echo "Error: unexpected fgets() fail\n";
+        }
+        fclose($handle);
+    }
+    $handle = @fopen("grammalecte.tsv", "r");
+    if ($handle) {
+        while (($line = fgets($handle, 4096)) !== false) {
+            if (strpos($line, "\tprn") === false) continue;
+            $data = str_getcsv($line, "\t");
+            $name = $data[0];
+            if (isset($forename[$name])) continue;
+            $cat = "NAMEpers";
+            if (strpos($line, "\tprn mas") !== false) $cat = "NAMEpersm";
+            else if (strpos($line, "\tprn fem") !== false) $cat = "NAMEpersf";
+            echo "$name,$cat\n";
+        }
+        if (!feof($handle)) {
+            echo "Error: unexpected fgets() fail\n";
+        }
+        fclose($handle);
+    }
+}
+
